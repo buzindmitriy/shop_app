4ca42ad8e66bd2033b06d42b58fbd18d0a1775d5
Buzin Dmitriy <89111703879@mail.ru>
2025-02-05 21:37:44 +0300

    add urls

---
+++ catalog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from catalog.apps import CatalogConfig
+from . import views
+
+app_name = CatalogConfig.name
+
+urlpatterns = [
+    path('home/', views.home_view, name='home'),
+    path('contacts/', views.contacts_view, name='contacts')
+]
