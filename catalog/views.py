b5082d324db74c606826a59af4033c83242386fc
Buzin Dmitriy <89111703879@mail.ru>
2025-02-05 21:36:56 +0300

    add controllers

---
+++ catalog/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home_view(request):
+    return render(request, 'home.html')
+
+
+def contacts_view(request):
+    return render(request, 'contacts.html')
+
+
+def contact_us(request):
+    if request.method == 'POST':
+        # Получение данных из формы
+        name = request.POST.get('name')
+        message = request.POST.get('message')
+        # Обработка данных (например, сохранение в БД, отправка email и т. д.)
+        print(name)
+        print(message)
+        # Здесь мы просто возвращаем простой ответ
+        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
+    return render(request, 'contact.html')
