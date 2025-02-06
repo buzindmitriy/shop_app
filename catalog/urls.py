a1f9ee748f931393f0eecfcab52dac69a8051a3a
Buzin Dmitriy <89111703879@mail.ru>
2025-02-07 01:10:44 +0300

    update urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('home/', views.home_view, name='home'),
-    path('contacts/', views.contacts_view, name='contacts')
+    path("home/", views.home_view, name="home"),
+    path("contacts/", views.contacts_view, name="contacts"),
 ]
