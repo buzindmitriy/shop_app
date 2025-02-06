b0971bc66fa3f16652c4c13ab47c79ecb1897614
Buzin Dmitriy <89111703879@mail.ru>
2025-02-07 01:08:29 +0300

    create models Product and Category

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Категория")
+    description = models.TextField(blank=True, verbose_name="Описание категории")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Продукт")
+    description = models.TextField(verbose_name="Описание продукта")
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="Продукты"
+    )
+    image = models.ImageField(
+        upload_to="images/", verbose_name="Изображение", blank=True, null=True
+    )
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    created_at = models.DateField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateField(auto_now=True, verbose_name="Дата изменения")
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["category", "name"]
+
+    def __str__(self):
+        return self.name
