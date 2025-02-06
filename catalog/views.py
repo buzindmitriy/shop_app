55ad4c3fc643979ec46038afb0198387ec4230c5
Buzin Dmitriy <89111703879@mail.ru>
2025-02-07 01:13:09 +0300

    blacked files

--- catalog/views.py
+++ catalog/views.py
@@ -3,21 +3,21 @@ from django.shortcuts import render
 
 
 def home_view(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
 
 
 def contacts_view(request):
-    return render(request, 'contacts.html')
+    return render(request, "contacts.html")
 
 
 def contact_us(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         # Получение данных из формы
-        name = request.POST.get('name')
-        message = request.POST.get('message')
+        name = request.POST.get("name")
+        message = request.POST.get("message")
         # Обработка данных (например, сохранение в БД, отправка email и т. д.)
         print(name)
         print(message)
         # Здесь мы просто возвращаем простой ответ
         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
-    return render(request, 'contact.html')
+    return render(request, "contact.html")
