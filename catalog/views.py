2a2cc44f20c3702843721d74890f9f0fc5400b50
Buzin Dmitriy <89111703879@mail.ru>
2025-02-09 16:45:20 +0300

    add_product views

--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from catalog.models import Product, Category
 
 
-def home_view(request):
-    return render(request, "home.html")
+def products_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "products_list.html", context)
 
 
-def contacts_view(request):
-    return render(request, "contacts.html")
+def products_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'products_detail.html', context)
 
 
 def contact_us(request):
@@ -20,4 +26,26 @@ def contact_us(request):
         print(message)
         # Здесь мы просто возвращаем простой ответ
         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
-    return render(request, "contact.html")
+    return render(request, "contacts.html")
+
+
+def add_product(request):
+    if request.method == 'POST':
+        # Получение данных из формы
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        price = request.POST.get('price')
+        category_id = request.POST.get('category')
+        image = request.FILES.get('image')  # Загрузка изображения продукта (если оно есть)
+        # Создание нового продукта
+        product = Product.objects.create(
+            name=name,
+            description=description,
+            price=price,
+            category_id=category_id,
+            image=image,
+        )
+        return HttpResponse(f"Продукт '{product.name}' успешно добавлен.")
+    # Формирование формы добавления продукта
+    categories = Category.objects.all()
+    return render(request, 'add_product.html', {'categories': categories})
