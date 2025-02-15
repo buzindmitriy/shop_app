5c30fff70371a21e389c2b3a360d13d98598fc2d
Buzin Dmitriy <89111703879@mail.ru>
2025-02-15 17:37:18 +0300

    update urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -19,11 +19,12 @@ urlpatterns = [
         name="products_detail",
     ),
     path("catalog/contacts/", ContactTemplateView.as_view(), name="contacts"),
+
     path("catalog/create/", ProductCreateView.as_view(), name="product_create"),
     path(
         "catalog/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"
     ),
     path(
         "catalog/<int:pk>/delete/", ProductDeleteView.as_view(), name="delete_product"
-    ),
+    )
 ]
